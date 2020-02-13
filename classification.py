b11437412f87440004c6902a4fa0bef03ff077f9
G3om <luca.romanini@usherbrooke.ca>
2020-02-12 22:24:09 -0500

    - Fonctions ajoutées pour .CSV

--- classification.py
+++ classification.py
@@ -14,34 +14,38 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import fiona
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 
 # Necessite aussi plusieurs dépendances...
 
-fichier_shp = r'E:\OneDrive - USherbrooke\001 APP\Programmation\inputs\tpi_david\zone_depots_glaciolacustre_31H02NE_MTM8.shp'
-seg_poly = gpd.read_file(fichier_shp)
+# Pour importer un shapefile
+#fichier_shp = r'E:\OneDrive - USherbrooke\001 APP\Programmation\inputs\tpi_david\zone_depots_glaciolacustre_31H02NE_MTM8.shp'
+#seg_poly = gpd.read_file(fichier_shp)
+
+# Import de .csv
+csv_metrique = pd.read_csv(r'E:\OneDrive - USherbrooke\001 APP\Programmation\inputs\raster_input\RelTPI_WB_31H02SE.csv')
 
 # seg_poly.plot()
 # plt.show()
 
 #print(seg_poly.columns) # Montrer le nom des colonnes
+print(csv_metrique.columns)
 
 # On choisi la colonne que l'on veut prédire (ici le type de dépots)
-y_depots = seg_poly.CODE_DEPOT
+#y_depots = seg_poly.CODE_DEPOT
+y_depots = csv_metrique.zone_y_n
 
 # On definit les métriques sur lesquels on veut faire l'analyse
-metriques = ['_count', '_sum', '_mean', '_median', '_stdev', '_min', '_max', '_range', '_minority', '_majority']
-X_metriques = seg_poly[metriques]
+#metriques = ['_count', '_sum', '_mean', '_median', '_stdev', '_min', '_max', '_range', '_minority', '_majority']
+#X_metriques = seg_poly[metriques]
 
-# print(X_metriques.describe())
-# print(X_metriques.head())
+X_metriques = csv_metrique.valeur_tpi
+X_metriques = X_metriques.to_numpy()
+X_metriques = X_metriques.reshape(-1, 1)
 
 # Séparation des données en données d'entrainement et données de tests
 train_metriques, test_metriques, train_y, test_y = train_test_split(X_metriques, y_depots, test_size = 0.25, random_state = 42)
-
-# print('Training Features Shape:', train_metriques.shape)
-# print('Training Labels Shape:', train_y.shape)
-# print('Testing Features Shape:', test_metriques.shape)
-# print('Testing Labels Shape:', test_y.shape)
 
 # Create a Gaussian Classifier
 clf = RandomForestClassifier(n_estimators = 100, verbose = 2, oob_score = True, random_state = 42)
@@ -56,20 +60,25 @@ print("Accuracy:", metrics.accuracy_score(test_y, y_pred))
 
 # Matrice de confusion
 #pd.crosstab()
+#c_matrice = confusion_matrix(test_y, y_pred)
 
-# Creer une nouvelle colonne dans le shapefile
-liste_des_resultats = list(zip(test_y, y_pred))
-df = pd.DataFrame({'id': test_y.index, 'prediction': y_pred})
+c_matrice = confusion_matrix(test_y, y_pred)
 
-# On ajoute les prédictions au futur shapefile
-for i, j in seg_poly.iterrows():
-    for k, l in df.iterrows():
-        if i == l['id']:
-            seg_poly.loc[i, 'prediction'] = l['prediction']
+disp = plot_confusion_matrix(clf, test_metriques, test_y,
+                             display_labels = y_pred,
+                             cmap = plt.cm.Blues,
+                             values_format='d')
 
-# On crée le shapefile avec les prédictions
-seg_poly.to_file("result_prediction.shp") # Vérifier fiona
+# # Creer une nouvelle colonne dans le shapefile
+# liste_des_resultats = list(zip(test_y, y_pred))
+# df = pd.DataFrame({'id': test_y.index, 'prediction': y_pred})
 
-
-
+# # On ajoute les prédictions au futur shapefile
+# for i, j in seg_poly.iterrows():
+#     for k, l in df.iterrows():
+#         if i == l['id']:
+#             seg_poly.loc[i, 'prediction'] = l['prediction']
+#
+# # On crée le shapefile avec les prédictions
+# seg_poly.to_file("result_prediction.shp") # Vérifier fiona
 
