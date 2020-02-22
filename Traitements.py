f264ec2f406b449ad81509776fc81bf878fea93b
JulioCortisone <Chuck782>
2020-02-21 21:36:51 -0500

    pas encore complet

--- Traitements.py
+++ Traitements.py
@@ -13,63 +13,104 @@ temp = os.path.join(main_dir, 'temp')
 
 # Resampling et correction hydrologique
 # Parcours les MNT 1x1 dans le dossier_MNT
-for parent, dossier, fichier in os.walk(dossier_MNT):
-    for mnts in fichier:
+# for parent, dossier, fichier in os.walk(dossier_MNT):
+#     for mnts in fichier:
+#
+#     # Path du MNT en cours
+#         mnt = os.path.join(parent, mnts)
+#
+#     # Création des répertoire de sortie
+#         mnt_5x5 = os.path.join(temp, 'MNT_5x5_{}.tif'.format(mnts[4:11]))
+#         mnt_cor = os.path.join(main_dir, 'MNT_5x5_cor', '{}'.format(mnts[4:9]), 'MNT_5x5_cor_{}.tif'.format(mnts[4:11]))
+#
+#
+#     # Resampling en 5x5 cubic spline
+#         resampling_cubic_spline(mnt, mnt_5x5, 5)
+#
+#     # Filtrage du MNT 5x5 FPDEMS
+#         fpdems(mnt_5x5, mnt_cor)
+#         #gdal_translate_32to64(fpdems32, mnt_cor)
+#
+#     # Suppresion des éléments du répertoire temporaire
+#         for file in os.listdir(temp):
+#             os.remove(os.path.join(temp,file))
+#
+# else:
+#     print('Tous les fichiers ont été reéchantillonnés')
 
-    # Path du MNT en cours
-        mnt = os.path.join(parent, mnts)
 
-    # # Création des répertoire de sortie
-    #     mnt_5x5 = os.path.join(temp, 'MNT_5x5_{}.tif'.format(mnts[4:11]))
-    #     mnt_corr = os.path.join(main_dir, 'MNT_5x5_corr', '{}'.format(mnts[4:9]), 'MNT_5x5_corr_{}.tif'.format(mnts[4:11]))
-    #
-    # # Resampling en 5x5 cubic spline
-    #     resampling_cubic_spline(mnt, mnt_5x5, 5)
-    #
-    # # Correction hydrologique, breach depressions
-    #     breachDepression(mnt_5x5, mnt_corr)
-    #
-    # # Suppresion des éléments du répertoire temporaire
-    #     for file in os.listdir(temp):
-    #         os.remove(os.path.join(temp,file))
-
-else:
-    print('Tous les fichiers ont été reéchantillonnés')
-
-
-# Creation TPI, TWI et pentes
-for parent, dossier, fichier in os.walk(os.path.join(os.path.join(main_dir, 'MNT_5x5_corr'))):
+# Creation des métriques
+for parent, dossier, fichier in os.walk(os.path.join(os.path.join(main_dir, 'MNT_5x5_cor'))):
     for image in fichier:
 
     # Image de base input
         print('Ouverture image {}'.format(image))
         mnt55 = os.path.join(parent, image)
 
+    # Repertoire de sortie des metrique
+        dir_metrique = os.path.join(main_dir, 'Metriques', '{}'.format(image[12:17]), '{}'.format(image[12:19]))
+
     # Création du TPI relatif avec un kernel de 200
-        TPI = os.path.join(main_dir, 'TPI_WB', image[13:18], 'RelTPI_WB_{}.tif'.format(image[13:20]))
+        TPI = os.path.join(dir_metrique, 'RelTPI_WB_{}'.format(image[12:19]))
         relative_TPI(mnt55, TPI, 40)
 
-    # # Création du SCA
-    #     sca = os.path.join(temp, 'SCA_{}.tif'.format(image[13:20]))
-    #     SCA(mnt55, sca)
-    #
-    # # Création de la pente
-    #     pente = os.path.join(main_dir, 'Pente_WB', image[13:18], 'Pente_WB_{}.tif'.format(image[13:20]))
-    #     slope(mnt55, pente)
-    #
-    # # Creation du TWI
-    #     twi = os.path.join(main_dir, 'TWI_WB', image[13:18], 'TWI_WB_{}.tif'.format(image[13:20]))
-    #     TWI(pente, sca, twi)
+    # Correction hydrologique, breach depressions pour le calcul du SCA
+        MNTBreachDepression = os.path.join(temp, 'MNT_BD_{}.tif'.format(image[12:19]))
+        breachDepression(mnt55, MNTBreachDepression)
+
+    # Création du SCA
+        sca = os.path.join(temp, 'SCA_{}.tif'.format(image[12:19]))
+        SCA(MNTBreachDepression, sca)
+
+    # Création de la pente
+        pente = os.path.join(dir_metrique, 'Pente_WB_{}.tif'.format(image[12:19]))
+        slope(mnt55, pente)
+
+    # Creation du TWI
+        twi = os.path.join(dir_metrique, 'TWI_WB_{}.tif'.format(image[12:19]))
+        TWI(pente, sca, twi)
+
+    # Creation du plan Curvature
+        planCur = os.path.join(dir_metrique, 'PlanCur_WB_{}.tif'.format(image[12:19]))
+        plan_curvature(mnt55, planCur)
+
+    # Creation du profile Curvature
+        profCur = os.path.join(dir_metrique, 'ProfCur_WB_{}.tif'.format(image[12:19]))
+        profile_curvature(mnt55, profCur)
+
+    # Creation du tangential Curvature
+        tanCur = os.path.join(dir_metrique, 'tanCur_WB_{}.tif'.format(image[12:19]))
+        tan_curvature(mnt55, tanCur)
+
+    # Creation du Circular Variance of Aspect
+        cirVar = os.path.join(dir_metrique, 'CirVarAsp_WB_{}.tif'.format(image[12:19]))
+        CircularVarofAspect(mnt55, cirVar)
+
+    # Création du Spherical Std Deviation of Normals
+        sphStd = os.path.join(dir_metrique, 'SphStdDevNor_WB_{}.tif'.format(image[12:19]))
+        sphericalStdDevNormals(mnt55,sphStd)
+
+    # Création du Edge Density
+        edgeDensity = os.path.join(dir_metrique, 'EdgeDens_WB_{}.tif'.format(image[12:19]))
+        EdgeDensity(mnt55, edgeDensity)
+
+    # Création du Downslope Index
+        DownslopeInd = os.path.join(dir_metrique, 'DownslopeInd_WB_{}.tif'.format(image[12:19]))
+        Downslope_Ind(MNTBreachDepression, DownslopeInd)
+
+    # Création du Average Normal Vector Angular Deviation
+        avrNor = os.path.join(dir_metrique, 'AvrNorVecAngDev_WB_{}.tif'.format(image[12:19]))
+        AverNormVectAngDev(mnt55, avrNor)
+
+
 
     # Suppresion des éléments du répertoire temporaire
         for file in os.listdir(temp):
             os.remove(os.path.join(temp,file))
 
-    # Clip de la couche de dépôts
-
 
 else:
-    print('Tous les TPI, Pentes et TWI ont été générés')
+    print('Toutes les métriques ont été créées')
 
 
 
