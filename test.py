f6fd1123a1ac4fef20180e5912bf0097d3827a91
JulioCortisone <Chuck782>
2020-02-21 21:36:45 -0500

    pas encore complet

--- test.py
+++ test.py
@@ -7,13 +7,173 @@ import whitebox
 import os
 import numpy as np
 import math
+import pandas as pd
+from scipy import stats
+import statistics as s
+from rasterio import features
+import pprint
+import sys
+from osgeo import gdal
+from osgeo import ogr
+
+path_mask = r'D:\test_mask_binaire.tif'
+path_qgis = r'C:\ProgramData\Anaconda3\envs\GDAL_test\Library\python\qgis'
+path_mnt = r'D:\DATA\MNT\31H02\MNT_31H02NE.tif'
+# path_mnt = r'D:\DATA\MNT_5x5_corr\31H02\MNT_5x5_corr_31H02NE.tif'
 
 
 
 
-def round_decade_up(n, decimals=0):
-    multiplier = 10 ** decimals
-    return math.ceil(n * multiplier) / multiplier
+
+# wb = whitebox.WhiteboxTools()
+#
+# tpi = os.path.join(os.path.dirname(path_mnt), 'TPI_31H02NO.tif')
+# wb.relative_topographic_position(path_mnt, tpi, 40)
+
+
+# # ouverture du mnt original
+# mnt_original = gdal.Open(path_mnt, gdal.GA_Update)
+# original_band = mnt_original.GetRasterBand(1)
+#
+# # Valeur NoData du mnt original
+# nodata_value = original_band.GetNoDataValue()
+# print('La valeur no data est: {}'.format(nodata_value))
+
+# # Lissage du mnt original
+# path_mnt_lisse = r'D:\smooth.tif'
+# wb.feature_preserving_smoothing(path_mnt, path_mnt_lisse)
+#
+# # ouverture du mnt lissé
+# mnt_filtre = gdal.Open(path_mnt_lisse, gdal.GA_Update)
+# lisse_band = mnt_filtre.GetRasterBand(1)
+#
+# # Déclaration de la valeur nodata dans le fichier output
+# lisse_band.SetNoDataValue(nodata_value)
+# lisse_nodata_value = lisse_band.GetNoDataValue()
+# print('La valeur no data est: {}'.format(lisse_nodata_value))
+
+
+# Ne fonctionne pas.
+
+
+
+# sys.path.append(path_qgis)
+# from qgis.core import *
+#
+#
+# gdal.UseExceptions()
+#
+# #
+# #  get raster datasource
+# #
+#
+# type_mapping = {gdal.GDT_Byte: ogr.OFTInteger,
+#                 gdal.GDT_UInt16: ogr.OFTInteger,
+#                 gdal.GDT_Int16: ogr.OFTInteger,
+#                 gdal.GDT_UInt32: ogr.OFTInteger,
+#                 gdal.GDT_Int32: ogr.OFTInteger,
+#                 gdal.GDT_Float32: ogr.OFTReal,
+#                 gdal.GDT_Float64: ogr.OFTReal,
+#                 gdal.GDT_CInt16: ogr.OFTInteger,
+#                 gdal.GDT_CInt32: ogr.OFTInteger,
+#                 gdal.GDT_CFloat32: ogr.OFTReal,
+#                 gdal.GDT_CFloat64: ogr.OFTReal}
+#
+# dataset = gdal.Open(path_mask)
+# srcband = dataset.GetRasterBand(1)
+# prj = dataset.GetProjection()
+# dst_layername = r'D:\test_mask_poly'
+# drv = ogr.GetDriverByName("ESRI Shapefile")
+# dst_ds = drv.CreateDataSource( dst_layername + ".shp" )
+# srs = ogr.osr.SpatialReference(wkt=prj)
+#
+# dst_layer = dst_ds.CreateLayer(dst_layername, srs=srs)
+# raster_field = ogr.FieldDefn('id', type_mapping[srcband.DataType])
+# a = gdal.Polygonize(srcband, None, dst_layer, -1, [], callback=None)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Extractions des statistiques
+
+
+# path_data  = r'D:\DATA\echantillons_metriques'
+# path_output = r'D:\DATA\echantillons_metriques\stats_31H02NE.csv'
+#
+#
+# index = ['Min int', 'Min ext', 'Max int', 'Max ext', 'Moyenne int', 'Moyenne ext', 'Écart type int', 'Écart type ext',
+#          'Médiane int', 'Médiane ext', 'Skewness int', 'Skewness ext', 'Kurtosis int', 'Kurtosis ext']
+#
+# data = pd.DataFrame(index=index)
+#
+#
+# for file in os.listdir(path_data):
+#
+#     nom = file.split('_')[0]
+#     csv = os.path.join(path_data, file)
+#     df = pd.read_csv(csv, index_col=False)
+#
+#     out_zone = df['out_zone']
+#     in_zone = df['in_zone']
+#
+#     max_in = in_zone.max()
+#     max_out = out_zone.max()
+#
+#     min_in = in_zone.min()
+#     min_out = out_zone.min()
+#
+#     mean_in = s.mean(in_zone)
+#     mean_out = s.mean(out_zone)
+#
+#     sd_in = s.stdev(in_zone)
+#     sd_out = s.stdev(out_zone)
+#
+#     med_in = s.median(in_zone)
+#     med_out = s.median(out_zone)
+#
+#     skew_in = stats.skew(in_zone)
+#     skew_out = stats.skew(out_zone)
+#
+#     kurt_in = stats.kurtosis(in_zone)
+#     kurt_out = stats.kurtosis(out_zone)
+#
+#     col = [min_in, min_out, max_in, max_out, mean_in, mean_out, sd_in, sd_out, med_in, med_out, skew_in, skew_out,
+#            kurt_in, kurt_out]
+#
+#     data[nom] = col
+#
+# print(data)
+# data.to_csv(path_output)
+#
+
+
+
 
 
 
@@ -61,3 +221,5 @@ def round_decade_up(n, decimals=0):
 #
 # print(liste_choix)
 # #
+
+
