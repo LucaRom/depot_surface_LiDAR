d74cb1a7f5c8a3ee33704f1e4c4e942f46a616e9
JulioCortisone <Chuck782>
2020-02-21 21:36:38 -0500

    pas encore complet

--- fonctions_traitements.py
+++ fonctions_traitements.py
@@ -3,6 +3,7 @@ import os
 import whitebox
 
 
+
 def resampling_cubic_spline(input, output, size):
 
     # Création des répertoire de sortie
@@ -23,9 +24,12 @@ def resampling_cubic_spline(input, output, size):
     print()
 
 
+
 def breachDepression(dem, output):
 
     wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
     # Création des répertoire de sortie
     head_output = os.path.dirname(output)
     if not os.path.exists(head_output):
@@ -105,3 +109,166 @@ def TWI(slope, sca, output):
     print('Terminé')
     print()
 
+
+def fpdems(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Filtrage
+    print('Filtrage du mnt...')
+    wbt.feature_preserving_smoothing(dem, output)
+    print('Terminé')
+    print()
+
+
+def gdal_translate_32to64(input, output):
+
+    # Création des répertoire de sortie
+    head_output = os.path.dirname(output)
+    if not os.path.exists(head_output):
+        os.makedirs(head_output)
+
+    # ouverture des images et extraction des dimensions
+    print('Ouverture {}'.format(input))
+    dataset = gdal.Open(input, gdal.GA_ReadOnly)
+    largeur, hauteur = (dataset.RasterXSize, dataset.RasterYSize)
+
+    # Resampling
+    print('Resampling...')
+    translate_object = gdal.TranslateOptions(width=largeur, height=hauteur, outputType=gdal.GDT_Float64)
+    gdal.Translate(destName=output, srcDS=input, options=translate_object)
+    print('Terminé')
+    print()
+
+
+def CircularVarofAspect(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du CircularVarianceOfAspect
+    print('Creation du CircularVarianceOfAspect...')
+    wbt.circular_variance_of_aspect(dem, output)
+    print('Terminé')
+    print()
+
+
+def EdgeDensity(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du Edge Density
+    print('Creation du EdgeDensity...')
+    wbt.edge_density(dem, output)
+    print('Terminé')
+    print()
+
+
+def sphericalStdDevNormals(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du Spherical Standard Deviation of Normals
+    print('Creation du Spherical Standard Deviation of Normals...')
+    wbt.spherical_std_dev_of_normals(dem, output)
+    print('Terminé')
+    print()
+
+
+def plan_curvature(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du plan curvature
+    print('Creation du plan curvature...')
+    wbt.plan_curvature(dem, output)
+    print('Terminé')
+    print()
+
+
+def profile_curvature(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du profile curvature
+    print('Creation du profile curvature...')
+    wbt.profile_curvature(dem, output)
+    print('Terminé')
+    print()
+
+
+def tan_curvature(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du tan curvature
+    print('Creation du tan curvature...')
+    wbt.tangential_curvature(dem, output)
+    print('Terminé')
+    print()
+
+
+def Downslope_Ind(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Création du Downslope Index
+    print('Creation du Downslope Index...')
+    wbt.downslope_index(dem, output)
+    print('Terminé')
+    print()
+
+
+def AverNormVectAngDev(dem, output):
+
+    wbt = whitebox.WhiteboxTools()
+    wbt.verbose = False
+
+    # Création du répertoire de sortie
+    if not os.path.exists(os.path.dirname(output)):
+        os.makedirs(os.path.dirname(output))
+
+    # Average Normal Vector Angular Deviation
+    print('Creation du Average Normal Vector Angular Deviation ...')
+    wbt.average_normal_vector_angular_deviation(dem, output)
+    print('Terminé')
+    print()
