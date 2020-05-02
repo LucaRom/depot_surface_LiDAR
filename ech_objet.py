17e57c6d2b0eb0c41282ce8e6808eb467c8f0dee
JulioCortisone <david.ethier@hotmail.ca>
2020-05-02 15:42:43 -0400

    Éechantillonnage + stats zonale + sauvegarde + commentaires

--- ech_objet.py
+++ ech_objet.py
@@ -12,11 +12,9 @@ poly = gpd.read_file(path_poly)
 depot = gpd.read_file(path_depot)
 
 # On peut appeler les méthodes des GeoSeries sur le geodataframe.
-# Pour intersects, on doit sépcifier avec quel objet de la serie on veut une comparaison.
+# Pour within, on doit sépcifier avec quel objet de la serie on veut une comparaison.
 # Le iloc retourne une série Pandas standard, il faut donc spécifier la colonne geometry pour avoir une GeoSerie
 # https://github.com/geopandas/geopandas/issues/317
-
-#print(poly.intersects(depot['geometry'].iloc[0]))
 
 # On crée la série booléenne pour vérifier quelles polygones sont contenus dans la zone de dépôts
 within = poly.within(depot['geometry'].iloc[0])
@@ -27,12 +25,19 @@ poly['Zone'] = within.astype(int)
 
 # STATISTIQUES ZONALES
 
+# Chemin du répertoire contenant les métriques
 path_met = r'C:\Users\home\Documents\Documents\APP2\Metriques\31H02\31H02NE'
 
+# Pour chaque polygones, on extrait les statistiques de chaque métriques
 for met in os.listdir(path_met):
+
+    # On crée le préfixe de la colonne selon le nom de la métrique
     path = os.path.join(path_met, met)
     prefixe = met.split('_')[0]
+
+    # On crée les stats zonales
     stats = zonal_stats(path_poly, path, stats=['min', 'max', 'median', 'mean', 'std', 'count'])
+    # On formatte les statistiques pour avoir une colonne (liste) par statistique
     col = {'min': [], 'max': [], 'median': [], 'mean': [], 'std': [], 'count': []}
     for i in stats:
         col['min'].append(i['min'])
@@ -42,10 +47,12 @@ for met in os.listdir(path_met):
         col['std'].append(i['std'])
         col['count'].append(i['count'])
 
+    # On ajoute les colonnes des stats dans la couche des polygones
     for c in col.keys():
         name = '{}_{}'.format(prefixe, c)
         poly[name] = col[c]
 
+# On sauvegarde la couche
 poly.to_file(path_poly)
 
 
