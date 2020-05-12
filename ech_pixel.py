15f55e1f25da82b820033bc86812006dae86cd1b
JulioCortisone <david.ethier@hotmail.ca>
2020-05-12 00:00:55 -0400

    organisation en fonction + line profiler

--- ech_pixel.py
+++ ech_pixel.py
@@ -3,17 +3,13 @@ import random
 from fiona.crs import from_epsg
 from shapely.geometry import shape, Point
 from shapely.ops import nearest_points
+import shapely.speedups
 
-#path = r'C:\Users\home\Documents\Documents\APP2\polygon_test.shp'
-path = r'C:\Users\home\Documents\Documents\APP2\depots_31H02\zone_depots_glaciolacustre_31H02NE_MTM8_reg.shp'
-value = 2000
-minDistance = 500
-
-
+#@profile
 def check_min_distance(point, distance, points):
+
     if distance == 0:
         return True
-
     if len(points) > 0:
         uni = points.unary_union
         nearest = nearest_points(point, uni)
@@ -21,60 +17,73 @@ def check_min_distance(point, distance, points):
             return True
         if nearest[1].distance(point) < distance:
             return False
-
     return True
 
-poly = gpd.read_file(path)
-epsg = int(poly.crs['init'].split(':')[1])
-sample = gpd.GeoDataFrame()
-sample['geometry'] = None
-sample['id'] = None
-sample.crs = from_epsg(epsg)
 
-total = 100.0 / len(poly) if len(poly) else 0
-for ind, row in poly.iterrows():
-    geom = row['geometry']
-    bbox = shape(geom).bounds
-    index = sample.sindex
-    #points = dict()
-    nPoints = 0
-    nIterations = 0
-    maxIterations = value * 50
-    feature_total = total / value if value else 1
+#@profile
+def echantillon_pixel(input, minDistance, value, output):
 
-    random.seed()
-    pointId = 0
-    while nIterations < maxIterations and nPoints < value:
+    poly = gpd.read_file(input)
+    epsg = int(poly.crs['init'].split(':')[1])
+    sample = gpd.GeoDataFrame(columns=['geometry', 'id'])
+    sample.crs = from_epsg(epsg)
 
-        height = bbox[3] - bbox[1]
-        width = bbox[2] - bbox[0]
-        rx = bbox[0] + width * random.random()
-        ry = bbox[1] + height * random.random()
+    for ind, row in poly.iterrows():
+        geom = row['geometry']
+        bbox = shape(geom).bounds
+        index = sample.sindex
+        nPoints = 0
+        nIterations = 0
+        maxIterations = value * 5
 
-        p = Point(rx, ry)
-        # geom = QgsGeometry.fromPointXY(p)
-        if p.within(geom) and \
-                (not minDistance or check_min_distance(p, minDistance, index)):
-            index.loc[nPoints, 'geometry'] = p
-            index.loc[nPoints, 'id'] = pointId
-            print(sample)
+        random.seed()
+        pointId = 0
+        while nIterations < maxIterations and nPoints < value:
 
-            # f = QgsFeature(nPoints)
-            # f.initAttributes(1)
-            # f.setFields(fields)
-            # f.setAttribute('id', pointId)
-            # f.setGeometry(geom)
-            # sink.addFeature(f, QgsFeatureSink.FastInsert)
-            # points[nPoints] = p
-            nPoints += 1
-            pointId += 1
-            # feedback.setProgress(current_progress + int(nPoints * feature_total))
-        nIterations += 1
+            height = bbox[3] - bbox[1]
+            width = bbox[2] - bbox[0]
+            rx = bbox[0] + width * random.random()
+            ry = bbox[1] + height * random.random()
+
+            p = Point(rx, ry)
+
+            if p.within(geom) and (not minDistance or check_min_distance(p, minDistance, sample)):
+                    sample.loc[nPoints, 'geometry'] = p
+                    sample.loc[nPoints, 'id'] = pointId
+                    nPoints += 1
+                    pointId += 1
+            nIterations += 1
+            if nIterations % 1000 == 0:
+                print(nIterations)
 
         if nPoints < value:
+            print(sample)
             print('Could not generate requested number of random points. Maximum number of attempts exceeded.')
 
-sample.to_file(r'C:\Users\home\Documents\Documents\APP3\test_points.shp')
+    sample.to_file(output)
+
+#@profile
+def main():
+
+    shapely.speedups.enable()
+    # Path de la couche à échantillonner
+    path = r'C:\Users\home\Documents\Documents\APP2\depots_31H02\zone_depots_glaciolacustre_31H02NE_MTM8_reg.shp'
+    # Nombre de points
+    value = 2000
+    # Distance minimale en unité du système de la couche
+    minDistance = 500
+    # Path de sauvegarde de l'échantillonnage
+    output = r'C:\Users\home\Documents\Documents\APP3\test_points.shp'
+    # Échantillonnage
+    #echantillon_pixel(input=path, minDistance=minDistance, value=value, output=output)
+
+    shp = gpd.read_file(path)
+    print(shp.loc[0, 'geometry'])
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 # orig = Point(1, 1.67)
