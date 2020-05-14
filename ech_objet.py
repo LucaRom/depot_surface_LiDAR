abee103a3e2f50d131c8535c72f0b2383f9f656a
G3om <luca.romanini@usherbrooke.ca>
2020-05-13 22:08:24 -0400

    - Ajout texte Help - enlevé 2-3 lignes

--- ech_objet.py
+++ ech_objet.py
@@ -2,6 +2,42 @@ import geopandas as gpd
 from rasterstats import zonal_stats
 import os
 
+#### DÉFINITION DES FONCTIONS ####
+# Import de tous les shapefiles d'un dossier et on les regroupe dans un seul fichier
+def set_root_chm():
+    # On définit le dossier parent pour le réutiliser dans l'import d'intrants
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+
+    return root_dir
+
+# def import_merge_seg():
+#     # Chemin vers le dossier avec les shapefiles
+#     folder_path = os.path.join(root_dir, 'inputs/segmentations/seg_Scikit_mai2020_31H02_NE_SE')
+#
+#     # On crée la liste des shapefiles
+#     files = os.listdir(folder_path)  # Liste des fichiers dans le dossier "folder"
+#     shp_list = [os.path.join(folder_path, i) for i in files if i.endswith('.shp')]  # Obtenir une liste des chemins pour
+#                                                                                     # .shp seulement
+#     # On join les fichiers .shp de la liste
+#     new_shp_temp = gpd.GeoDataFrame(pd.concat([gpd.read_file(i) for i in shp_list],
+#                                               ignore_index=True), crs=gpd.read_file(shp_list[0]).crs)
+#
+#     return new_shp_temp # Retourne le shapefile
+
+def set_chemins():
+    # On ajoute la date au fichier sortant pour suivre nos tests
+    date_classi = str(datetime.now().strftime('%Y_%m_%d_%H_%M_%S'))
+
+    # Chemins de la couche de segmentation et de la couche de dépôts
+    path_segmentation = os.path.join(root_dir, 'inputs/segmentations/seg_Scikit_mai2020_31H02_NE_SE/')
+    path_depot = os.path.join(root_dir, 'inputs/depots/31H02NE/zone_depots_glaciolacustre_31H02NE_MTM8_reg.shp')
+
+    # Chemin du répertoire contenant les métriques
+    path_met = os.path.join(root_dir, 'inputs/tiffs/31H02NE_5m/')
+
+    # chemin de la couche de sortie
+    nom_fichier = 'result_prediction_SEG_sickit' + date_classi + '.shp'
+    output = os.path.join(root_dir, 'outputs/Segmentations/31H02NE_5m', nom_fichier)
 
 def stats_zonales(path_metriques, path_segmentation):
 
@@ -54,16 +90,6 @@ def echantillon_objet(path_depot, segmentation):
 
 def main():
 
-    # Chemins de la couche de segmentation et de la couche de dépôts
-    path_segmentation = r'C:\Users\home\Documents\Documents\APP2\polygon_test.shp'
-    path_depot = r'C:\Users\home\Documents\Documents\APP2\Depots_31H\Depots_31H\zone_depots_glaciolacustre_31H02NE_MTM8_reg.shp'
-
-    # Chemin du répertoire contenant les métriques
-    path_met = r'C:\Users\home\Documents\Documents\APP2\Metriques\31H02\31H02NE'
-
-    # chemin de la couche de sortie
-    output = r'C:\Users\home\Documents\Documents\APP2\polygon_test.shp'
-
     # Statistiques zonales
     print('Calcul des statistiques zonales...')
     seg_stats = stats_zonales(path_metriques=path_met, path_segmentation=path_segmentation)
@@ -77,8 +103,10 @@ def main():
     seg_stats_ech.to_file(output)
     print('Terminé')
 
-
+#### INITIATION DU SCRIPT ####
 if __name__ == "__main__":
+    set_root_chm()
+    set_chemins()
     main()
 
 
