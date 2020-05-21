8f7eb0826133a67dba0ce6cf81482c99e0e82f50
JulioCortisone <david.ethier@hotmail.ca>
2020-05-21 00:18:58 -0400

    Download des 9 MNT adjacents pour un feuillet donné

--- Download_MNT.py
+++ Download_MNT.py
@@ -4,106 +4,105 @@ David Ethier, Yves Voirin,  2020
 
 import os
 from ftplib import FTP
+import geopandas as gpd
 
 
-# intrants
-feuillet = '31H'
-feuillet_debut = '31H01'
-feuillet_fin = '31H02'
+def selection_mnt(feuillet, path_index, col_feuillet):
 
-# répertoire output
-output = r'D:\DATA\MNT'
-
-# Lien du serveur du MFFP pour les images du feuillet
-ftpdirectory = 'transfert.mffp.gouv.qc.ca' # site ftp
-parent = r'Public/Diffusion/DonneeGratuite/Foret/IMAGERIE/Produits_derives_LiDAR/' # répertoire de base
+    index = gpd.read_file(path_index)
+    geom_feuillet = index['geometry'][index[col_feuillet] == feuillet].values[0]
+    index['intersect'] = index.intersects(geom_feuillet)
+    liste_download = (list(index[col_feuillet][index['intersect'] == True].values))
+    return liste_download
 
 
-parent = os.path.join(parent, feuillet)
+def download_ftp(ftpparent, ftpdirectory, liste_files, rep_output):
+    for i in liste_files:
+        if os.path.exists(os.path.join(output, 'MNT_{}.tif'.format(i))):
+            liste_files.remove(i)
+    print(liste_files)
+    with FTP(ftpdirectory) as ftp:
+        ftp.login()
+        ftp.retrlines("LIST")
+        ftp.cwd(ftpparent)
+        listing = []
+        ftp.retrlines("MLSD", listing.append)
+
+        liste_rep_ordre = sorted([i.split(';') for i in listing], key=lambda liste: liste[-1])
+
+        for rep in liste_rep_ordre:
+
+            if rep[0].strip() == 'Type=dir' and rep[-1].strip() in [i[:3] for i in liste_files]:
+                num = rep[-1].strip()
+
+                print('{} dans la liste des répertoire voulus'.format(num))
+                print('Parcours du repertoire {}'.format(num))
+
+                ftp.cwd(rep[-1].strip())
+                liste_file = []
+                ftp.retrlines("MLSD", liste_file.append)
+
+                liste_feuillet_ordre = sorted([i.split(';') for i in liste_file], key=lambda liste: liste[-1])
+
+                for rep_feuillet in liste_feuillet_ordre:
+
+                    if rep_feuillet[0].strip() == 'Type=dir' and rep_feuillet[-1].strip() in liste_files:
+
+                        num_rep = rep_feuillet[-1].strip()
+
+                        print('{} dans la liste des MNT voulus'.format(num_rep))
+                        print('Parcours du repertoire {}'.format(num_rep))
+
+                        ftp.cwd(rep_feuillet[-1].strip())
+                        liste_file = []
+                        ftp.retrlines("MLSD", liste_file.append)
+
+                        for files in liste_file:
+                            info_file = files.split(';')
+                            fichier_mnt = info_file[-1].strip()
+
+                            if fichier_mnt == 'MNT_{}.tif'.format(num_rep):
+
+                                print('fichier MNT: {} trouvé'.format(fichier_mnt))
+                                print('début du téléchargement')
+
+                                #output_dir = os.path.join(rep_output, num_rep[:5])
+                                if not os.path.exists(output):
+                                    print('création du répertoire {}'.format(output))
+                                    os.makedirs(output)
+
+                                local_filename = os.path.join(output, fichier_mnt)
+                                lf = open(local_filename, "wb")
+                                ftp.retrbinary("RETR " + fichier_mnt, lf.write, 8 * 1024)
+                                lf.close()
+
+                                print('Fin du téléchargement')
+                                print()
+
+                        ftp.cwd('..')
 
 
-def liste_mnt_a_download(feuillet, div_debut, div_fin):
+def download_mnt(feuillet, path_index, col_feuillet, ftpparent, ftpdirectory, output):
 
-    liste_voulu = []
-    if feuillet[:3] == div_debut[:3] and feuillet[:3] == div_fin[:3]:
+    # Création de la liste des MNT à télécharger
+    liste = selection_mnt(feuillet, path_index, col_feuillet)
 
-        nb_deb = int(div_debut[3:5])
-        nb_fin = int(div_fin[3:5])
-        nb_fois = nb_fin - nb_deb + 1
-
-        compte_feuillet = nb_deb
-        for i in range(nb_fois):
-            liste_div = ['NE','NO','SE','SO']
-            for j in liste_div:
-                compte = None
-                if compte_feuillet < 10:
-                    compte = '0'+ str(compte_feuillet)
-                else:
-                    compte = str(compte_feuillet)
-
-                a = '{}{}{}'.format(feuillet, compte, j)
-                liste_voulu.append(a)
-            compte_feuillet += 1
-
-        return liste_voulu
+    # Téléchargement des MNT
+    download_ftp(ftpparent, ftpdirectory, liste, output)
 
 
-liste_voulu = liste_mnt_a_download(feuillet, feuillet_debut, feuillet_fin)
-print('liste des MNT voulues: {}'.format(liste_voulu))
-print()
+if __name__ == '__main__':
 
+    # Lien du serveur du MFFP pour les images du feuillet
+    ftpdirectory = 'transfert.mffp.gouv.qc.ca' # site ftp
+    ftpparent = r'Public/Diffusion/DonneeGratuite/Foret/IMAGERIE/Produits_derives_LiDAR/' # répertoire de base
 
-with FTP(ftpdirectory) as ftp:
-    ftp.login()
-    ftp.retrlines("LIST")
-    ftp.cwd(parent)
-    listing = []
-    ftp.retrlines("MLSD", listing.append)
+    path_index = r'C:\Users\home\Documents\Documents\APP3\Index_produit_derive_lidar\Index_produit_derive_lidar\Index_ProduitsDerive_LiDAR_Sweb.shp'
+    col_feuillet = 'FCA_NO_FEU'
+    feuillet = '31H02NE'
 
-    liste_feuillet_ordre = sorted([i.split(';') for i in listing], key=lambda liste: liste[-1])
+    # répertoire output
+    output = r'C:\Users\home\Documents\Documents\APP2\mnt\31H02NE'
 
-    for rep_feuillet in liste_feuillet_ordre:
-
-        if rep_feuillet[0].strip() == 'Type=dir' and rep_feuillet[-1].strip() in liste_voulu :
-            num_rep = rep_feuillet[-1].strip()
-
-            print('{} dans la liste des MNT voulus'.format(num_rep))
-            print('Parcours du repertoire {}'.format(num_rep))
-
-            ftp.cwd(rep_feuillet[-1].strip())
-            liste_file = []
-            ftp.retrlines("MLSD", liste_file.append)
-
-            for files in liste_file:
-                info_file = files.split(';')
-                fichier_mnt = info_file[-1].strip()
-
-                if fichier_mnt == 'MNT_{}.tif'.format(num_rep):
-
-                    print('fichier MNT: {} trouvé'.format(fichier_mnt))
-                    print('début du téléchargement')
-
-                    output_dir = os.path.join(output, num_rep[:5])
-                    if not os.path.exists(output_dir):
-                        print('création du répertoire {}'.format(output_dir))
-                        os.mkdir(output_dir)
-
-                    local_filename = os.path.join(output_dir, fichier_mnt)
-                    lf = open(local_filename, "wb")
-                    ftp.retrbinary("RETR " + fichier_mnt, lf.write, 8 * 1024)
-                    lf.close()
-
-                    print('Fin du téléchargement')
-                    print()
-
-            ftp.cwd('..')
-
-
-
-# wbt = whitebox.WhiteboxTools()
-# RUST_BACKTRACE=1
-# path = r'D:\DATA\raster'
-# wbt.set_working_dir(path)
-# wbt.verbose = False
-# output = os.path.join(path, 'tpi_wb_python.tif')
-# wbt.relative_topographic_position('MNT_31H02NE_5X5.tif', output, 200, 200)
+    download_mnt(feuillet=feuillet, path_index=path_index, col_feuillet=col_feuillet,
+                 ftpparent=ftpparent, ftpdirectory=ftpdirectory, output=output)
