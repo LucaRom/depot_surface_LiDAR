78f02eb1c1ce572d0c25f283a67d2a8bd6475d51
JulioCortisone <david.ethier@hotmail.ca>
2020-05-25 13:25:39 -0400

    version 25 mai

--- haralick.R
+++ haralick.R
@@ -1,41 +1,57 @@
-setwd('C:/Users/home/Documents/Documents/APP2')
-path_NE <- 'MNT_31H02NE_5x5.tif'
-path_SE <- 'MNT_31H02SE_5x5.tif'
-path_SO <- 'MNT_31H02SO_5x5.tif'
+#!/usr/bin/env Rscript
+
+# On extrait l'argument contenant le repertoire de travail
+args <- commandArgs(trailingOnly = TRUE)
+
+# On identifie les arguments en entrée
+input <- args[1]
+output <- args[2]
+metrique <- args[3]
+kernel <- strtoi(args[4])
+
+
+# On verifie si les packages necessaires sont installes, sinon on les intstalle
+print('Vérification des packages nécessaire..')
+packages <- c("raster", "rgdal", "sp", "glcm")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  print('installation...')
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
 
 require(raster)
 require(rgdal)
+require(glcm)
 
 
-# Mean_SE <- glcm(raster(path_ras, layer=1), window = c(39,39), statistics = c('mean'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-# plot(Mean_SE)
-# writeRaster(Mean_SE, filename = 'Mean_31H02SE.tif', format='GTiff', overwrite = TRUE)
-# 
-# Contrast_SE <- glcm(raster(path_ras, layer=1), window = c(39,39), statistics = c('contrast'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-# plot(Contrast_SE)
-# writeRaster(Contrast_SE, filename = 'Contrast_31H02SE.tif', format='GTiff', overwrite = TRUE)
+# On identifie le traitement à effectuer selon la texture choisie
+traitement = ''
 
-# correl_NE <- glcm(raster(path_NE, layer=1), window = c(39,39), statistics = c('correlation'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-# plot(correl_NE)
-# writeRaster(correl_NE, filename = 'correl_GLCM_31H02NE.tif', format='GTiff', overwrite = TRUE)
-# 
-# correl_SE <- glcm(raster(path_SE, layer=1), window = c(39,39), statistics = c('correlation'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-# plot(correl_SE)
-# writeRaster(correl_SE, filename = 'correl_GLCM_31H02SE.tif', format='GTiff', overwrite = TRUE)
+if(metrique==1){
+  traitement = 'mean'
+}
+if(metrique==2){
+  traitement = 'correlation'
+}
+if(metrique==3){
+  traitement = 'contrast'
+}
 
 
+# fonction du calcul des textures d'haralick
+haralick <- function(input, output, traitement, kernel){
+  # Calcul de la texture
+  print(sprintf('Calcul de la metrique: %s...', traitement))
+  texture <- glcm(raster(input, layer=1),
+                  window = c(kernel,kernel),
+                  statistics = c(traitement),
+                  shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
+                  
+  # Création du raster de sortie
+  print('Sauvegarde...')
+  writeRaster(texture, filename = output, format='GTiff', overwrite = TRUE)
+  print('Terminé')
+}
 
-# Feuillet 31H02SO
-
-Contrast_SO <- glcm(raster(path_SO, layer=1), window = c(39,39), statistics = c('contrast'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-plot(Contrast_SO)
-writeRaster(Contrast_SO, filename = 'Contrast_GLCM_31H02SO.tif', format='GTiff', overwrite = TRUE)
-
-Mean_SO <- glcm(raster(path_SO, layer=1), window = c(39,39), statistics = c('mean'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-plot(Mean_SO)
-writeRaster(Mean_SO, filename = 'Mean_GLCM_31H02SO.tif', format='GTiff', overwrite = TRUE)
-
-correl_SO <- glcm(raster(path_SO, layer=1), window = c(39,39), statistics = c('correlation'), shift=list(c(0,1), c(1,1), c(1,0), c(1,-1)))
-plot(correl_SO)
-writeRaster(correl_SO, filename = 'correl_GLCM_31H02SO.tif', format='GTiff', overwrite = TRUE)
+# Appel de la fonction
+haralick(input, output, traitement, kernel)
 
